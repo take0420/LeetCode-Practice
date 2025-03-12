efcb39e0a8b0d60a4a9613373693bdd0cb23b61d
take0420 <zakisen81@gmail.com>
2025-03-12 09:54:14 +0900

    Prefix

--- Prefix_Sum/maximum_good_subarray_sum.py
+++ Prefix_Sum/maximum_good_subarray_sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-
-        # 累積和を事前計算
+        
+        # 累積和を事前計算する
         cumulative_sum = [0] * (n + 1)
         for i in range(n):
             cumulative_sum[i + 1] = cumulative_sum[i] + nums[i]
@@ -11,13 +11,13 @@ class Solution:
         last_seen_index = {}  # {数値: その数値が最後に出たインデックス}
 
         for i, num in enumerate(nums):
-            # 差の絶対値がkになる数値が過去に出たかチェック
+            # 差の絶対値がkになる数値が過去に出たかチェックする
             for target_val in (num - k, num + k):
                 if target_val in last_seen_index:
                     j = last_seen_index[target_val]
                     max_good_sum = max(max_good_sum, cumulative_sum[i + 1] - cumulative_sum[j])
 
-            # より大きい部分和を保持するための更新処理
+            # より大きい部分和を保持するための更新処理を行う
             if num in last_seen_index:
                 if cumulative_sum[i + 1] - cumulative_sum[last_seen_index[num]] < nums[i]:
                     last_seen_index[num] = i
