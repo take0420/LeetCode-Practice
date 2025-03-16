21dce89cf0358b4a6b427333dbc140b7958b607e
take0420 <zakisen81@gmail.com>
2025-03-16 10:45:33 +0900

    fix:

--- Two_Pointers/base.py
+++ Two_Pointers/base.py
@@ -1,5 +1,6 @@
 def is_palindrome(s: str) -> bool:
-  right, left = 0, len(s) - 1
+  left, right = 0, len(s) - 1
+
   while left < right: # pointer が交差するまで
     if s[left] != s[right]:
       return False
