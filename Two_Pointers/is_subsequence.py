fd8f708e62ecd56f5c508fc3c55d35caaaefce17
take0420 <zakisen81@gmail.com>
2025-03-17 09:56:13 +0900

    two pointers

--- Two_Pointers/is_subsequence.py
+++ Two_Pointers/is_subsequence.py
@@ -1,14 +1,11 @@
 class Solution:
-  def isSubsequence(self, s: str, t: str) -> bool:
-    s_ptr = t_ptr = 0
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_index, t_index = 0, 0
+        s_length, t_length = len(s), len(t)
 
-    while s_ptr < len(s) and t_prt < len(t):
-      if s[s_ptr] == t[t_ptr]:
-        s_ptr += 1
-
-      t_ptr += 1
-
-    return s_ptr == len(s)
-
-# T: O(min(len(s), len(t)))
-# S: O(1)
+        while s_index < s_length and t_index < t_length:
+            if s[s_index] == t[t_index]:
+                s_index += 1  # 一致したらsの次の文字に進む
+            t_index += 1  # 毎回tの次の文字に進む
+        
+        return s_index == s_length  # sを最後まで確認できたかチェック
