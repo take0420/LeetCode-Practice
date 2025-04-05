27fa80d9f4f137291274ac24247e8bd4a0d5e4b9
take0420 <zakisen81@gmail.com>
2025-04-05 12:50:03 +0900

    128. Longest Consecutive Sequence Simple Solution

--- Arrays_Hashing/longest_consective_simple.py
+++ Arrays_Hashing/longest_consective_simple.py
@@ -1,3 +1,14 @@
 class Solution:
   def longestConsecutive(self, nums: List[int]) -> int:
+    numSet = set(nums) # 重複の値は排除した集合
+    longest = 0
     
+    for num in numSet:
+      # num が連続する値の視点である場合のみカウントを開始する
+      if num - 1 not in numSet:
+        len = 1
+        while num + len in numSet:
+          len += 1
+        longest = max(longest, len)
+    
+    return longest
