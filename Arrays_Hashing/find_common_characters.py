8f39641bbef6cbc92eb93618ff30906682e5de73
take0420 <zakisen81@gmail.com>
2025-04-14 10:00:24 +0900

    recap

--- Arrays_Hashing/find_common_characters.py
+++ Arrays_Hashing/find_common_characters.py
@@ -1,15 +1,14 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
         cnt = Counter(words[0])
-
         for w in words:
             cur_cnt = Counter(w)
             for char in cnt:
                 cnt[char] = min(cnt[char], cur_cnt[char])
-        
+    
         ans = []
         for char in cnt:
-            for i in range(cnt[char]):
+            for _ in range(cnt[char]):
                 ans.append(char)
-
+    
         return ans
