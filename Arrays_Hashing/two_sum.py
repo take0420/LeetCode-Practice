cc7726ff0b711626fe5cd13b4ae5cd7309d6785d
take0420 <zakisen81@gmail.com>
2025-04-17 10:00:42 +0900

    chore

--- Arrays_Hashing/two_sum.py
+++ Arrays_Hashing/two_sum.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        num_map = {}
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in num_map:
+                return [num_map[diff], i]
+            num_map[num] = i
